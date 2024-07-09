1868cb4b5143b2de14795b8e2854d309272ff8ec
Dhvani Sheth <dhvanisheth25@gmail.com>
2024-07-09 11:17:08 -0400

    added bsuienss signup api for registering the businesses

--- TiffinBox/authentication.py
+++ TiffinBox/authentication.py
@@ -96,10 +96,37 @@ def loginWithPhone(request):
                 "status": "active",
                 "image": photoUrl
             }
-            user = db.child("Users").child(id).set(user_data)
+            db.child("Users").child(id).set(user_data)
+            user = db.child("Users").child(id).get().val()
+            print(user)
             return JsonResponse({"status": "success", "user": user})
         except Exception as e:
             return JsonResponse({"status": "error", "message": str(e)})
     else:
         return JsonResponse({"status": "success", "user": existingUser})
 
+
+@csrf_exempt
+def business_signup(request):
+    data = json.loads(request.body)
+    email = data.get("email")
+    password = data.get("password")
+    phone = data.get("phoneNumber")
+    business_name = data.get("businessName")
+
+    try:
+        user = auth.create_user_with_email_and_password(email, password)
+        user_data = {
+            "email": email,
+            "phone": phone,
+            "business_name": business_name,
+            "role": "business",
+            "status": "active",
+            "image": "",
+            "name": '',
+            "verified": False
+        }
+        user = db.child("Users").child(user["localId"]).set(user_data)
+        return JsonResponse({"status": "success", "user": user})
+    except Exception as e:
+        return JsonResponse({"status": "error", "message": str(e)})
